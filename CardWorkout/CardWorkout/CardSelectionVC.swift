272a546be6b128133ae8e7bbd8c196d8355caa77
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-01 14:52:02 +0300

    Rules Screen

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        for button in buttons {
+            button.layer.cornerRadius = 15
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
         
     }
-
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+        
+    }
 }
